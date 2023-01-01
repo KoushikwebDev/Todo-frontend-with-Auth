b92ae96365f35a8666831bc6eae33b7fc1139be5
KoushikwebDev <kou.saha.ami@gmail.com>
2023-01-01 19:38:39 +0530

    Bug fixed

--- src/Appwrite/appwrite.config.js
+++ src/Appwrite/appwrite.config.js
@@ -5,7 +5,7 @@ const client = new Client();
 const account = new Account(client);
 
 client
-  .setEndpoint("http://localhost:80/v1") // Your API Endpoint
-  .setProject("63ab226ca399822bb845"); // Your project ID
+  .setEndpoint("http://143.110.180.23/v1") // Your API Endpoint
+  .setProject("63b13d2f62aea3711e06"); // Your project ID
 
 export default account;
